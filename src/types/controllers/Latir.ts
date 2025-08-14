51ce71e4e84b0b2f85bf9ffe7aa186b37692cdb4
CodeHaus <suporteanalisando@gmail.com>
2025-08-14 09:30:04 -0300

    Refacting: Editando types de Latir

--- src/types/controllers/Latir.ts
+++ src/types/controllers/Latir.ts
@@ -16,7 +16,7 @@ namespace ControllerLatir {
         categoria: z4.string(),
         usuario_id: z4.number(),
         raca: z4.string(),
-        idade: z4.string(),
+        idade: z4.string()
     });
     export type LatirBase = z4.infer<typeof LatirBaseSchema>;
 
@@ -28,23 +28,28 @@ namespace ControllerLatir {
 
         export const OutputSchema = LatirBaseSchema;
         export type Output = {
-            data: z4.infer<typeof OutputSchema>;
+            data: {
+                latir: z4.infer<typeof OutputSchema>;
+            }
         }
     }
 
     export namespace BuscarPeloFiltro {
         export const InputSchema = z4.object({
             filtros: z4.object({
-                id: z4.number().optional(),
-                descricao: z4.string().optional(),
-                tipo: z4.string().optional(),
-                valor: z4.number().optional(),
-                data: z4.string().optional(),
-                categoria: z4.string().optional(),
-                usuario_id: z4.number().optional(),
-                raca: z4.string().optional(),
-                idade: z4.string().optional(),
-            }),
+                latir: z4.object({
+                    pagina: z4.number().min(0),
+                    id: z4.number().optional().nullable(),
+                    descricao: z4.string().optional().nullable(),
+                    tipo: z4.string().optional().nullable(),
+                    valor: z4.number().optional().nullable(),
+                    data: z4.string().optional().nullable(),
+                    categoria: z4.string().optional().nullable(),
+                    usuario_id: z4.number().optional().nullable(),
+                    raca: z4.string().optional().nullable(),
+                    idade: z4.string().optional().nullable()
+                }),
+            })
         });
 
         export type Input = z4.infer<typeof InputSchema>;
@@ -91,7 +96,7 @@ namespace ControllerLatir {
                     categoria: z4.string().optional(),
                     usuario_id: z4.number().optional(),
                     raca: z4.string().optional(),
-                    idade: z4.string().optional(),
+                    idade: z4.string().optional()
                 })
             })
         });
